05f93ac8168d639544cb5782e073b8b4efc1354b
Toninhopt <50358397+Toninhopt@users.noreply.github.com>
2019-06-22 19:42:35 +0100

    Criação classe para Testes JUnit
    Classe TESTES.java

---
+++ src/Trabalho/TESTES.java
@@ -0,0 +1,17 @@
+package Trabalho;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Trabalho.*;
+
+class TESTES {
+
+	@Test
+	void Mc() {
+		
+		fail("Not yet implemented");
+	}
+
+}
